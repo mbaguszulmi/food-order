508272fb2ac2174dc0fe7db39def7595061d47a2
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-01-17 15:34:07 +0700

    Fix nav link name and apply nav to app.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,66 +3,25 @@ import { Link } from "react-router-dom";
 import { Layout, Header, Drawer, Navigation} from "react-mdl";
 
 class Nav extends Component {
-    state = {}
-
-    onLocationChange(location) {
-        let hash = window.location.hash;
-        document.body.scrollTop = 0
-        document.documentElement.scrollTop = 0
-
-        if (hash === "#/") {
-            this.setState({
-                activeIdx: 0
-            })
-        }
-        else if (hash === "#/checkout") {
-            this.setState({
-                activeIdx: 1
-            })
-        }
-        else if (hash === "#/add-food" || hash.indexOf("#/project") !== -1) {
-            this.setState({
-                activeIdx: 2
-            })
-        }
-        else if (hash === "#/add-promo") {
-            this.setState({
-                activeIdx: 3
-            })
-        }
-        else if (hash === "#/promo-list") {
-            this.setState({
-                activeIdx: 4
-            })
-        }
-    }
-
-    componentDidMount() {
-        this.onLocationChange()
-        window.addEventListener("hashchange", () => {
-            this.onLocationChange()
-        })
-    }
-
     render() {
         return(
             <Layout fixedHeader>
                 <Header title="Food Order">
                     <Navigation>
-                        <Link class="active" to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/checkout">Checkout</Link>
+                        <Link to="/add-food">Add Food</Link>
+                        <Link to="/add-promo">Add Promo</Link>
+                        <Link to="/promo-list">Promo List</Link>
                     </Navigation>
                 </Header>
                 <Drawer title="Food Order">
                     <Navigation>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
-                        <Link to="#">Link</Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/checkout">Checkout</Link>
+                        <Link to="/add-food">Add Food</Link>
+                        <Link to="/add-promo">Add Promo</Link>
+                        <Link to="/promo-list">Promo List</Link>
                     </Navigation>
                 </Drawer>
             </Layout>
