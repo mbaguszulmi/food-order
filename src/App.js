ae400108321bf6b8333177fdcb9547d0fb6d8cf1
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-01-17 16:41:39 +0700

    add module redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Nav from './components/nav'
 import { Layout, Content } from "react-mdl";
 import MainRoute from "./routes/main-route";
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <Nav />
-        <Content>
-          <MainRoute />
-        </Content>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Layout>
+          <Nav />
+          <Content>
+            <MainRoute />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
