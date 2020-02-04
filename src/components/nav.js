3dd59adc1e56ff0c0c8763ffc2b494b9bc5aaffc
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-02-04 09:08:47 +0700

    hide drawer when menu item clicked

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,6 +3,18 @@ import { Link } from "react-router-dom";
 import { Header, Drawer, Navigation, Layout } from "react-mdl";
 
 class Nav extends Component {
+    componentDidMount() {
+        let drawer = document.querySelector('div.mdl-layout__drawer');
+        let obfuscator = document.querySelector('div.mdl-layout__obfuscator');
+        let drawerMenuItems = document.querySelectorAll('div.mdl-layout__drawer a.mdl-navigation__link');
+        drawerMenuItems.forEach(element => {
+            element.addEventListener('click', () => {
+                drawer.classList.remove("is-visible");
+                obfuscator.classList.remove("is-visible");
+            })
+        })
+    }
+
     render() {
         return(
             <Layout fixedHeader>
