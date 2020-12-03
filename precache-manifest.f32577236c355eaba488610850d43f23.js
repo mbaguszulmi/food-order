0d66c366ea8adcf938d8fb7496b84ab4b90dd508
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-12-03 21:01:04 +0700

    deploy v0.1.0

---
+++ precache-manifest.f32577236c355eaba488610850d43f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d541421d1e4ebde459cd1c24de780535",
+    "url": "/food-order/index.html"
+  },
+  {
+    "revision": "33ad9f6ad5f5b95a3066",
+    "url": "/food-order/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "96cb4302b6d110bd0fb0",
+    "url": "/food-order/static/css/main.728c2c71.chunk.css"
+  },
+  {
+    "revision": "33ad9f6ad5f5b95a3066",
+    "url": "/food-order/static/js/2.e15e0d2d.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/food-order/static/js/2.e15e0d2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96cb4302b6d110bd0fb0",
+    "url": "/food-order/static/js/main.39547e68.chunk.js"
+  },
+  {
+    "revision": "da18d62e648c72a0d2df",
+    "url": "/food-order/static/js/runtime-main.40d45be2.js"
+  }
+]);
